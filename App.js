5d01baf1d9f44c753d0b4471230f470d314b98f8
Jordan Rammelsberg <96778376+Jramm91@users.noreply.github.com>
2023-04-22 22:50:22 +0000

    Building tab navigation

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
 import React from "react"
-import { StyleSheet, View } from "react-native"
 import CurrentWeather from "./src/screens/CurrentWeather"
 import UpcomingWeather from "./src/screens/UpcomingWeather"
 import City from "./src/screens/City"
 import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
 
+const Tab = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <CurrentWeather />
-      </View>
+      <Tab.Navigator 
+      screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'grey'
+      }}>
+          <Tab.Screen name={'Current'} component={CurrentWeather} options={{tabBarIcon:}} />
+          <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
+          <Tab.Screen name={'City'} component={City} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 
 export default App
