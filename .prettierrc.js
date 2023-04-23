ce21e5adc0fd1b79bb3dc150a0144869ff925b38
Jordan Rammelsberg <96778376+Jramm91@users.noreply.github.com>
2023-04-23 15:14:00 +0000

    Added tabs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
-export const bracketSpacing = true;
-export const singleQuote = true;
-export const tabWidth = 2;
-export const useTabs = false;
-export const trailingComa = "none";
-export const semi = false;
+ module.exports = {
+    bracketSpacing: true,
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+    trailingComa: "none",
+    semi: false,
+}
