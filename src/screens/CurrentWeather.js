bdca7e735e46f8f4dd33d0dcea3ba0adfa513d98
Jordan Rammelsberg <96778376+Jramm91@users.noreply.github.com>
2023-04-24 19:21:39 +0000

    setting up dynamic weather

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -4,11 +4,11 @@ import { Feather } from '@expo/vector-icons'
 import RowText from '../components/RowText'
 import { weatherType } from '../utilities/weatherType'
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ weatherData }) => {
   const {
     wrapper,
     container,
-    temp,
+    tempStyles,
     feels,
     highLowWrapper,
     highLow,
@@ -16,22 +16,38 @@ const CurrentWeather = () => {
     description,
     message,
   } = styles
+
+  const {
+    main: { temp, feels_like, temp_max, temp_min },
+    weather
+  } = weatherData
+  const weatherCondition = weather[0].main
+
   return (
-    <SafeAreaView style={wrapper}>
+    <SafeAreaView
+      style={[
+        wrapper,
+        { backgroundColor: weatherType[weatherCondition].backgroundColor }
+      ]}
+    >
       <View style={container}>
-        <Feather name="sun" size={100} color="black" />
-        <Text style={temp}>6</Text>
-        <Text style={feels}>Feels like 5</Text>
+        <Feather
+          name={weatherType[weatherCondition].icon}
+          size={100}
+          color="white"
+        />
+        <Text style={tempStyles}>{temp}</Text>
+        <Text style={feels}>{`Feels like ${feels_like}`}</Text>
         <RowText 
-          messageOne={'High: 8 '} 
-          messageTwo={'Low: 6'} 
+          messageOne={`High: ${temp_max} `} 
+          messageTwo={`Low: ${temp_min}`} 
           containerStyles={highLowWrapper} 
           messageOneStyle={highLow} 
           messageTwoStyle={highLow} 
         />
       </View>
       <RowText 
-        messageOne={'It\'s Sunny'}
+        messageOne={weather[0].description}
         messageTwo={weatherType['Thunderstorm'].message}
         containerStyles={bodyWrapper}
         messageOneStyle={description}
@@ -51,7 +67,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  temp: {
+  tempStyles: {
     color: 'black',
     fontSize: 48
   },
